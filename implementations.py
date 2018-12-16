7ccec4515e0a68d4e54e44421680f881a38ad448
erdembocugoz <erdembocugoz@sabanciuniv.edu>
2018-12-16 07:25:04 +0100

    imp

--- implementations.py
+++ implementations.py
@@ -47,4 +47,19 @@ def evaluate(prediction, truth):
 
     return rmse
 
+def submission_table(original_df, col_userID, col_movie, col_rate):
+	""" return table according with Kaggle convention """
+
+	def id(row):
+	    return 'r' + str(int(row[col_userID])) + '_c' + str(int(row[col_movie]))
+
+	def pred(row):
+	    return row[col_rate]
+
+	df = pd.DataFrame.copy(original_df)
+	df['Id'] = df.apply(id, axis=1)
+	df['Prediction'] = df.apply(pred, axis=1)
+
+	return df[['Id', 'Prediction']]
+
 
