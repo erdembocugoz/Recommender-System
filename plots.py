4cbc15021dbd818b03077cb35546cb3b1d465a50
serifsonerserbest <43368426+serifsonerserbest@users.noreply.github.com>
2018-12-19 17:31:23 +0100

    -- u

--- plots.py
+++ plots.py
@@ -16,15 +16,15 @@ def plot_raw_data(ratings):
     # plot
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
-    ax1.plot(sorted_num_movies_per_user, color='blue')
-    ax1.set_xlabel("users")
-    ax1.set_ylabel("number of ratings (sorted)")
+    ax1.plot(sorted_num_movies_per_user, color='red')
+    ax1.set_xlabel("Users")
+    ax1.set_ylabel("# of Ratings (sorted)")
     ax1.grid()
 
     ax2 = fig.add_subplot(1, 2, 2)
-    ax2.plot(sorted_num_users_per_movie)
-    ax2.set_xlabel("items")
-    ax2.set_ylabel("number of ratings (sorted)")
+    ax2.plot(sorted_num_users_per_movie, color='red')
+    ax2.set_xlabel("Items")
+    ax2.set_ylabel("# of Ratings (sorted)")
     ax2.set_xticks(np.arange(0, 12000, 3000))
     ax2.grid()
 
@@ -43,37 +43,44 @@ def plot_train_test_data(train, test):
     ax1.set_xlabel("Users")
     ax1.set_ylabel("Items")
     ax1.set_title("Training data")
+    
     ax2 = fig.add_subplot(1, 2, 2)
     ax2.spy(test, precision=0.01, markersize= 0.5)
     ax2.set_xlabel("Users")
     ax2.set_ylabel("Items")
     ax2.set_title("Test data")
+    
     plt.tight_layout()
     plt.savefig("train_test")
     plt.show()
     
 def plot_mean_and_std_per_user(user_means, user_stds):
     data = user_means
-
-    bins = np.arange(0, 5, 0.05) # fixed bin size
-    plt.xlim([min(data) -0.5, max(data) + 0.5])
-    plt.hist(data, bins=bins, alpha=0.5)
-    plt.title('mean per user')
-    plt.xlabel('mean')
-    plt.ylabel('count')
-    plt.show()
+    bins = np.arange(0, 5, 0.1) # fixed bin size
+    
+    fig = plt.figure()
+    
+    ax1 = fig.add_subplot(1, 2, 1)
+    plt.xlim([min(data) - 0.5, max(data) + 0.5])
+    ax1.hist(data, bins=bins, alpha=0.5)
+    ax1.set_xlabel("Mean")
+    ax1.set_ylabel("# of Users")
+    #ax1.savefig("mean_per_user")
 
     data = user_stds
+    bins = np.arange(0, 2, 0.05) # fixed bin size
     
-    bins = np.arange(0, 2, 0.03) # fixed bin size
-
+    ax2 = fig.add_subplot(1, 2, 2)
     plt.xlim([min(data) - 0.3, max(data) + 0.3])
-
-    plt.hist(data, bins=bins, alpha=0.5)
-    plt.title('std per user')
-    plt.xlabel('std')
-    plt.ylabel('count')
+    ax2.hist(data, bins=bins, alpha=0.5)
+    ax2.set_xlabel("Standart Deviation")
+    ax2.set_ylabel("# of Users")
+    #ax2.savefig("std_per_user")
+    
+    plt.tight_layout()
+    plt.savefig("statss")
     plt.show()
+
 
 
 
